dc8ecefbbdff5c437e07363059320baabe2c73b6
jcacc2002 <122575451+jcacc2002@users.noreply.github.com>
2024-06-05 22:48:32 +0200

    Updated Two Scripts
    + Added all header and footer code so that I can use them as includes for easy access through all pages.

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CollectifyMT</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="main_feed.php">CollectifyMT</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="store.php">Store</a></li>
+            <li class="nav-item"><a class="nav-link" href="profile.php">Profile</a></li>
+            <li class="nav-item"><a class="nav-link" href="friends.php">Friends</a></li>
+            <li class="nav-item"><a class="nav-link" href="messages.php">Messages</a></li>
+            <li class="nav-item"><a class="nav-link" href="notifications.php">Notifications</a></li>
+            <li class="nav-item"><a class="nav-link" href="settings.php">Settings</a></li>
+        </ul>
+    </div>
+</nav>
