6266c3e79facc8a73507747068e3c78ded3a8c83
jcacc2002 <122575451+jcacc2002@users.noreply.github.com>
2024-06-06 17:16:23 +0200

    Updated Two Scripts
    + Properly connected style.css
    + Changed a bit the header

--- includes/header.php
+++ includes/header.php
@@ -5,21 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CollectifyMT</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
     <a class="navbar-brand" href="main_feed.php">CollectifyMT</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav ml-auto">
             <li class="nav-item"><a class="nav-link" href="profile.php">Profile</a></li>
             <li class="nav-item"><a class="nav-link" href="friends.php">Friends</a></li>
             <li class="nav-item"><a class="nav-link" href="messages.php">Messages</a></li>
             <li class="nav-item"><a class="nav-link" href="notifications.php">Notifications</a></li>
             <li class="nav-item"><a class="nav-link" href="settings.php">Settings</a></li>
+            <li class="nav-item"><a class="nav-link" href="handlers/logout_handler.php">Log Out</a></li>
         </ul>
     </div>
 </nav>
